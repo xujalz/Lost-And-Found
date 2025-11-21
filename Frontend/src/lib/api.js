635229915bfa4dfd4fc1b5d3b21ec83aaecefaff
Sujal Gupta <sujalgupta1104@gmail.com>
2025-11-21 13:19:22 +0530

    ninth commit

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // match your backend (you used /api)
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // match your backend (you used /api)
   withCredentials: true,
 });
 
